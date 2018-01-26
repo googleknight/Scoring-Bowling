f1751132a124874ff4b09c869b3b3a95814ea912
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-26 09:01:09 +0530

    Tested for Open frame,Comnbination of zero and non zero inputs

--- bowling.test.js
+++ bowling.test.js
@@ -7,5 +7,8 @@ describe('Calculate scores for only Open frame inputs', () => {
   it('All zero inputs', () => {
     expect(bowling.score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(0);
   });
+  it('Comnbination of zero and non zero inputs', () => {
+    expect(bowling.score([3, 0, 3, 6, 3, 6, 0, 6, 3, 6, 3, 6, 3, 6, 3, 0, 3, 6, 3, 6])).toBe(75);
+  });
 });
 

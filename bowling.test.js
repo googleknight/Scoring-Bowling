0aa4b37281e670b361fa9f3b09130c1ed338e8bb
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-26 09:00:23 +0530

    Tested for Open frames, all zero inputs

--- bowling.test.js
+++ bowling.test.js
@@ -4,5 +4,8 @@ describe('Calculate scores for only Open frame inputs', () => {
   it('All non zero inputs', () => {
     expect(bowling.score([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
   });
+  it('All zero inputs', () => {
+    expect(bowling.score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(0);
+  });
 });
 
